20e326e678811e431d063d542de62e306a0c570d
Dinesh Rout <rddinesh779@gmail.com>
2022-12-20 02:06:24 +0530

    added logo

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Flex,
-  HStack,
   useDisclosure,
   useMediaQuery,
   Drawer,
@@ -14,27 +13,29 @@ import {
   Spacer,
   Stack,
   Container,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import { HamburgerIcon } from "@chakra-ui/icons";
-import { useRef } from "react";
-import NavItemsGroup from "./NavItemsGroup";
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { useRef } from 'react';
+import NavItemsGroup from './NavItemsGroup';
 
 function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isMobile] = useMediaQuery("(max-width: 900px)");
+  const [isMobile] = useMediaQuery('(max-width: 900px)');
   const btnRef = useRef();
 
   return (
-    <Container maxW={"7xl"}>
-      <Flex py="4">
-        <Link href="/">
+    <Container maxW={'7xl'}>
+      <Flex py='4'>
+        <Link href='/'>
           <Button
-            variant="ghost"
-            _hover={{ background: "blue.100" }}
-            colorScheme="blue"
-            color="black"
-            size={"lg"}
+            variant='ghost'
+            colorScheme='blue'
+            color='#5879f1'
+            size={'xl'}
+            className='logo'
+            py='8px'
+            pr={'16px'}
           >
             Zigbee
           </Button>
@@ -47,22 +48,33 @@ function Navbar() {
             </Button>
             <Drawer
               isOpen={isOpen}
-              placement="left"
+              placement='left'
               onClose={onClose}
               finalFocusRef={btnRef.current}
-              size="full"
+              size='full'
             >
               <DrawerOverlay />
               <DrawerContent>
                 <DrawerCloseButton />
-                <DrawerHeader>Zigbee</DrawerHeader>
+                <DrawerHeader>
+                  <Button
+                    variant='ghost'
+                    colorScheme='blue'
+                    color='#5879f1'
+                    size={'1em'}
+                    className='logo'
+                    p='16px'
+                  >
+                    Zigbee
+                  </Button>
+                </DrawerHeader>
 
                 <DrawerBody>
                   <NavItemsGroup />
                 </DrawerBody>
 
                 <DrawerFooter>
-                  <Button variant="outline" mr={3} onClick={onClose}>
+                  <Button variant='outline' mr={3} onClick={onClose}>
                     Cancel
                   </Button>
                 </DrawerFooter>
