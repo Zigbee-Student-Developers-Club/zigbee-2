fbf94fee934a6590fe1b691ce77ba04daec2d3e7
Dinesh Rout <rddinesh779@gmail.com>
2022-12-23 13:40:58 +0530

    Dynamic FAQ section added

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -7,54 +7,43 @@ import {
   Box,
   Container,
   Heading,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
+
+const faqs = [
+  {
+    title: 'What is Zigbee?',
+    content: `Zigbee is a student run community run by OUTR MCA Students that focuses fundamentally on software development. Zigbee CETB subsists on the belief that everyone can create & innovate.`,
+  },
+];
 
 export default function FAQ() {
   return (
-    <Container my="4" py="4" maxW={"7xl"}>
+    <Container my='4' py='4' maxW={'7xl'}>
       <Heading
-        textAlign="center"
-        fontWeight="black"
-        fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-        as="h2"
+        textAlign='center'
+        fontWeight='black'
+        fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+        as='h2'
       >
         FAQs
       </Heading>
-      <Accordion allowToggle>
-        <AccordionItem border="none">
-          <h2>
-            <AccordionButton _expanded={{ bg: "teal.100", borderRadius: "md" }}>
-              <Box as="span" flex="1" textAlign="left">
-                Section 1 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem border="none">
-          <h2>
-            <AccordionButton _expanded={{ bg: "blue.100", borderRadius: "md" }}>
-              <Box as="span" flex="1" textAlign="left">
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      {faqs.map((faq, i) => (
+        <Accordion allowToggle key={i}>
+          <AccordionItem border='none'>
+            <h2>
+              <AccordionButton
+                _expanded={{ bg: 'teal.100', borderRadius: 'md' }}
+              >
+                <Box as='span' flex='1' textAlign='left'>
+                  {faq.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>{faq.content}</AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      ))}
     </Container>
   );
 }
