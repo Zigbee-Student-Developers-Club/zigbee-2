9610068dc179511d9e7351a5cff61347cdfc2c37
Dinesh Rout <rddinesh779@gmail.com>
2022-12-24 01:39:23 +0530

    added draft page

--- components/navbar/NavItemsGroup.tsx
+++ components/navbar/NavItemsGroup.tsx
@@ -1,42 +1,42 @@
-import { Button, Stack, useMediaQuery } from "@chakra-ui/react";
-import Link from "next/link";
+import { Button, Stack, useMediaQuery } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const navItemsData = [
   {
-    name: "Department",
-    route: "/department",
+    name: 'Department',
+    route: '/department',
   },
   {
-    name: "Alumni",
-    route: "/alumni",
+    name: 'Alumni',
+    route: '/alumni',
   },
   {
-    name: "Events",
-    route: "/events",
+    name: 'Events',
+    route: '/events',
   },
   {
-    name: "Resources",
-    route: "/resources",
+    name: 'Resources',
+    route: '/resources',
   },
   {
-    name: "Drafts",
-    route: "/Drafts",
+    name: 'Drafts',
+    route: '/drafts',
   },
 ];
 
 export default function NavItemsGroup() {
-  const [isMobile] = useMediaQuery("(max-width: 900px)");
+  const [isMobile] = useMediaQuery('(max-width: 900px)');
 
   return (
-    <Stack direction={isMobile ? "column" : "row"}>
+    <Stack direction={isMobile ? 'column' : 'row'}>
       {navItemsData.map((item, index) => (
         <Link href={item.route} key={index}>
           <Button
-            variant="ghost"
-            _hover={{ background: "blue.100" }}
-            colorScheme="blue"
-            color="black"
-            size={"lg"}
+            variant='ghost'
+            _hover={{ background: 'blue.100' }}
+            colorScheme='blue'
+            color='black'
+            size={'lg'}
           >
             {item.name}
           </Button>
