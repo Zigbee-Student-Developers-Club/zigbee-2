01fb07c0078c19a8c153020c5d09a2bc0e67abf7
Dinesh Rout <rddinesh779@gmail.com>
2023-02-26 20:52:09 +0530

    added event data

--- components/eventCard/EventCard.tsx
+++ components/eventCard/EventCard.tsx
@@ -8,9 +8,9 @@ import {
   Heading,
   Stack,
   Text,
-} from "@chakra-ui/react";
-import { NextPage } from "next";
-import Image from "next/image";
+} from '@chakra-ui/react';
+import { NextPage } from 'next';
+import Image from 'next/image';
 
 interface EventDataProps {
   data?: any;
@@ -20,33 +20,38 @@ const EventCard: NextPage<EventDataProps> = ({ data }) => {
   console.log(data.thumbnail);
   // console.log(data);
   return (
-    <Card maxW="sm" height="100%" m="auto" background={"gray.100"}>
+    <Card maxW='sm' height='100%' m='auto' background={'gray.100'}>
       <CardBody>
-        <Flex justifyContent={"center"}>
-          <Box overflow={"hidden"} rounded="lg">
+        <Flex justifyContent={'center'}>
+          <Box overflow={'hidden'} rounded='lg'>
             <Image
               src={data.thumbnail}
               alt={data.topic}
-              width="300"
-              height="250"
+              width='300'
+              height='250'
             />
           </Box>
         </Flex>
-        <Stack mt="4" spacing="1">
-          <Heading size="md">{data.speaker.name}</Heading>
-          <Text size="12px">{data.date}</Text>
-          <Text size="12px">{data.speaker.role}</Text>
-          <Text size="12px">{data.speaker.company}</Text>
-          <Heading size="md">{data.topic}</Heading>
+        <Stack mt='4' spacing='1'>
+          <Heading size='md' mb={'1'}>
+            {data.topic}
+          </Heading>
+          <Heading size='sm'>{data.speaker.name}</Heading>
+          <Text size='xs'>
+            {data.speaker.role}, {data.speaker.company}
+          </Text>
+          <Text size='xs' fontWeight={'bold'}>
+            {data.date}
+          </Text>
         </Stack>
       </CardBody>
-      <CardFooter justifyContent={"center"} p="2">
+      <CardFooter justifyContent={'center'} p='2'>
         <Button
-          width={"100%"}
-          colorScheme={data.expired ? "red" : "green"}
-          variant="ghost"
+          width={'100%'}
+          colorScheme={data.expired ? 'red' : 'green'}
+          variant='ghost'
         >
-          {data.expired ? "Event Expired" : data.eventDate}
+          {data.expired ? 'Event Expired' : data.date}
         </Button>
       </CardFooter>
     </Card>
--- pages/alumniConnect.tsx
+++ pages/alumniConnect.tsx
@@ -7,41 +7,41 @@ import {
   Heading,
   Stack,
   Text,
-} from "@chakra-ui/react";
-import EventCard from "components/eventCard/EventCard";
-import Footer from "components/footer/Footer";
-import EventHeroImg from "assets/event-img.png";
-import Navbar from "components/navbar/Navbar";
-import Salesforce from "assets/salesforce.png";
-import Game from "assets/game.png";
-import Image from "next/image";
+} from '@chakra-ui/react';
+import EventCard from 'components/eventCard/EventCard';
+import Footer from 'components/footer/Footer';
+import EventHeroImg from 'assets/event-img.png';
+import Navbar from 'components/navbar/Navbar';
+import Salesforce from 'assets/salesforce.png';
+import Game from 'assets/game.png';
+import Image from 'next/image';
 
 const eventsData = [
   {
     id: 1,
-    date: "28th Nov 2022",
+    date: '28th Nov 2022',
+    batch: '2011',
     speaker: {
-      role: "Tech Lead",
-      name: "Debasis Jena",
-      company: "Dubai Multi Commodities Center",
+      role: 'Tech Lead',
+      name: 'Debasis Jena',
+      company: 'Dubai Multi Commodities Center',
     },
-    topic: "Cloud Application Development with Salesforce Platform",
+    topic: 'Cloud Application Development with Salesforce Platform',
     expired: true,
     thumbnail: Salesforce.src,
-    eventDate: "",
   },
   {
     id: 2,
-    date: "31st Dec 2022",
+    date: '24th Jan 2023',
+    batch: '2020',
     speaker: {
-      role: "Game Developer",
-      name: "Mallik Ebadat",
-      company: "Amgo Games",
+      role: 'Game Developer',
+      name: 'Mallik Ebadat',
+      company: 'Amgo Games',
     },
-    topic: "Game Development with Unity 3D",
-    expired: false,
+    topic: 'Game Development with Unity 3D',
+    expired: true,
     thumbnail: Game.src,
-    eventDate: "31 December 2022",
   },
 ];
 
@@ -49,21 +49,21 @@ export default function AlumniConnect() {
   return (
     <>
       <Navbar />
-      <Container maxW={"7xl"}>
+      <Container maxW={'7xl'}>
         <Stack
-          align={"center"}
+          align={'center'}
           spacing={{ base: 8, md: 10 }}
-          direction={{ base: "column", md: "row" }}
-          backgroundColor={"purple.100"}
-          borderRadius="2xl"
-          py="4"
-          px="14"
+          direction={{ base: 'column', md: 'row' }}
+          backgroundColor={'purple.100'}
+          borderRadius='2xl'
+          py='4'
+          px='14'
         >
           <Stack flex={1} spacing={{ base: 5, md: 10 }}>
             <Heading
-              fontWeight="black"
-              fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-              as="h2"
+              fontWeight='black'
+              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+              as='h2'
             >
               Events
             </Heading>
@@ -74,31 +74,31 @@ export default function AlumniConnect() {
               ventures in the recent past.
             </Text>
           </Stack>
-          <Flex flex={1} justify={"center"} align={"center"}>
+          <Flex flex={1} justify={'center'} align={'center'}>
             <Image
-              height="200"
-              width="400"
-              alt="zigbee hero"
+              height='200'
+              width='400'
+              alt='zigbee hero'
               src={EventHeroImg}
             />
           </Flex>
         </Stack>
 
         <Grid
-          justifyContent={"center"}
-          alignContent="center"
-          gridAutoRows={"1fr"}
+          justifyContent={'center'}
+          alignContent='center'
+          gridAutoRows={'1fr'}
           templateColumns={{
-            base: "repeat(1, 1fr)",
-            md: "repeat(2, 1fr)",
-            lg: "repeat(3, 1fr)",
-            xl: "repeat(4, 1fr)",
+            base: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+            lg: 'repeat(3, 1fr)',
+            xl: 'repeat(4, 1fr)',
           }}
           gap={8}
-          my={"10"}
+          my={'10'}
         >
           {eventsData.map((data: any, i) => (
-            <GridItem key={i} justifyContent={"center"} alignContent="center">
+            <GridItem key={i} justifyContent={'center'} alignContent='center'>
               <EventCard data={data} />
             </GridItem>
           ))}
