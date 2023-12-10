e7ff08adb4e7e8d33fa3eb796cef740727994541
Dinesh Rout <rddinesh779@gmail.com>
2023-12-11 01:36:28 +0530

    style: UI changes

--- components/eventCard/EventCard.tsx
+++ components/eventCard/EventCard.tsx
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { Calendar, Calendar2, Person } from 'react-bootstrap-icons';
 
 interface EventDataProps {
   data?: any;
@@ -25,29 +26,50 @@ const EventCard: NextPage<EventDataProps> = ({ data }) => {
         <Flex justifyContent={'center'}>
           <Box overflow={'hidden'} rounded='lg'>
             <Image
+              style={{ minWidth: '100%', aspectRatio: 16 / 9 }}
+              width={'360'}
               src={data.thumbnail}
               alt={data.topic}
-              width='300'
               height='250'
             />
           </Box>
         </Flex>
-        <Stack mt='4' spacing='1'>
-          <Heading size='md' mb={'1'}>
+        <Box mt='4'>
+          <Heading size='md' mb={'2'}>
             {data.topic}
           </Heading>
-          {data.speaker && (
-            <>
-              <Heading size='sm'>{data.speaker.name}</Heading>
-              <Text size='xs'>
-                {data.speaker.role}, {data.speaker.company}
-              </Text>
-            </>
-          )}
-          <Text size='xs' fontWeight={'bold'}>
-            {data.date}
-          </Text>
-        </Stack>
+          {data.speakers &&
+            data.speakers.map((s: any, i: any) => (
+              <Box key={i} my={4}>
+                <Flex gap={2}>
+                  <Flex
+                    style={{ aspectRatio: 1 }}
+                    w={12}
+                    h={12}
+                    bg={'cyan.400'}
+                    justifyContent='center'
+                    alignItems={'center'}
+                    borderRadius={'8'}
+                  >
+                    <Person size={'25'} />
+                  </Flex>
+                  <Box>
+                    <Heading size='sm'>{s.name}</Heading>
+                    <Text size='xs'>
+                      {s.role}, {s.company}
+                    </Text>
+                  </Box>
+                </Flex>
+              </Box>
+            ))}
+
+          <Flex gap={2} alignItems={'center'}>
+            <Calendar2 size={'18'} />
+            <Text size='18px' fontWeight={'bold'}>
+              {data.date}
+            </Text>
+          </Flex>
+        </Box>
       </CardBody>
       <CardFooter justifyContent={'center'} p='2'>
         <Button
