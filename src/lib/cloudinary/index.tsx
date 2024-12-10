3044e0840194b8c46179d44df064e25b29e49783
Prabodh Kumar Panda <dev.prabodhpanda@gmail.com>
2024-12-10 20:48:11 +0530

    feat: added cloudinary upload mechanism

---
+++ src/lib/cloudinary/index.tsx
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import { Readable } from "stream";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadProfileToCloudinary = async (
+  fileBuffer: Buffer,
+  fileName: string
+): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: "profiles",
+        public_id: fileName.split(".")[0],
+      },
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(result);
+      }
+    );
+
+    const bufferStream = new Readable();
+    bufferStream.push(fileBuffer);
+    bufferStream.push(null);
+    bufferStream.pipe(uploadStream);
+  });
+};
