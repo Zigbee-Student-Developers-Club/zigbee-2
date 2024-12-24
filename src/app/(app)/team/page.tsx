274f288d994bdb43c53c5b270358b4bcb326be9d
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-12-25 02:46:38 +0530

    style: contributor page

--- src/app/(app)/team/page.tsx
+++ src/app/(app)/team/page.tsx
@@ -32,36 +32,37 @@ export default function ContributorsPage() {
 
   return (
     <>
-    <MotionDivProvider>
-      <div className="mx-auto max-w-7xl px-4">
-        {/* InfoSection */}
-        <InfoSection
-          imageSrc="/about-us.webp"
-          heading="Our Team"
-          text="We’re designers, developers, strategists, basically coding maniacs. We’re funny, innovative, presumably procrastinators and mostly awkward dancers. Yes, we’re Zigbee OUTR, a dynamic developers and coding community, aimed at raising the bar of the coding culture in and around us. We’re here by the students and we’re here for the students, with our base located in the premises of the Odisha University of Technology and Research, Bhubaneswar."
-          background="bg-emerald-100"
-          darkBackground="dark:bg-emerald-900"
-          placedImage={true}
-        /></div>
-          </MotionDivProvider>
-
-        {/* Title */}
-        <Title size="medium" className="mb-10 text-center text-3xl font-bold">
-          Our Contributors
-        </Title>
-
-        {/* Grid Section */}
-        <div className="mx-auto grid max-w-7xl  mb-12 grid-cols-1 gap-8 px-10 sm:grid-cols-2 lg:grid-cols-4">
-          {contributors?.map((contributor, index: number) => (
-            <ContributorCard
-              key={index}
-              name={contributor?.name || "Anonymous"} // Fallback for missing names
-              profileImage={contributor?.profileImg || "/default-avatar.jpg"} // Fallback for missing profile images
-              linkedinURL={contributor?.linkedInUrl || "#"} // Fallback for missing LinkedIn URLs
-              batch={contributor?.batch?.toString() || "N/A"} // Fallback for missing batch
-            />
-          ))}
+      <MotionDivProvider>
+        <div className="mx-auto">
+          {/* InfoSection */}
+          <InfoSection
+            imageSrc="/about-us.webp"
+            heading="Our Team"
+            text="We’re designers, developers, strategists, basically coding maniacs. We’re funny, innovative, presumably procrastinators and mostly awkward dancers. Yes, we’re Zigbee OUTR, a dynamic developers and coding community, aimed at raising the bar of the coding culture in and around us. We’re here by the students and we’re here for the students, with our base located in the premises of the Odisha University of Technology and Research, Bhubaneswar."
+            background="bg-emerald-100"
+            darkBackground="dark:bg-emerald-900"
+            placedImage={true}
+          />
         </div>
-  </>
+      </MotionDivProvider>
+
+      {/* Title */}
+      <Title size="medium" className="my-10 text-center text-3xl font-bold">
+        Our Contributors
+      </Title>
+
+      {/* Grid Section */}
+      <div className="mx-auto mb-12 grid max-w-7xl grid-cols-1 gap-8 px-3 sm:grid-cols-2 lg:grid-cols-4">
+        {contributors?.map((contributor, index: number) => (
+          <ContributorCard
+            key={index}
+            name={contributor?.name || "Anonymous"} // Fallback for missing names
+            profileImage={contributor?.profileImg || "/default-avatar.jpg"} // Fallback for missing profile images
+            linkedinURL={contributor?.linkedInUrl || "#"} // Fallback for missing LinkedIn URLs
+            batch={contributor?.batch?.toString() || "N/A"} // Fallback for missing batch
+          />
+        ))}
+      </div>
+    </>
   );
 }
